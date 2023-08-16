046dd5d620fd6a773161c675a31ea1cb69901fdd
T-Yohan <yohanturlet@outlook.fr>
2023-08-16 13:15:50 -0400

    login page ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+//@ts-ignore
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import COVER_IMAGE from './Beach.jpg'
+
+const colors = {
+  primary: "#060606",
+  background: "#E0E0E0",
+  disbaled: "#D9D9D9"
 }
 
-export default App;
+const Login = () => {
+  return (
+    <div className='w-full h-screen flex items-start'>
+      <div className='relative w-1/2 h-full flex flex-col'>
+
+<div className='absolute top=[25%] left=[10%] flex flex-col'>
+  <h1 className='text-4xl text-white font-bold my-4'>Turn Your Dreams into reality</h1>
+  <p className='text-xl text-white font-normal'> Start for free and get attractive offers from the community</p>
+</div>
+
+
+        <img src={COVER_IMAGE} className='w-full h-full object-cover'/>
+      </div>
+    </div>
+  )
+}
+// function App() {
+//   return (
+//     <div className="App">
+// Hello World!
+//     </div>
+//   );
+// }
+
+export default Login;
